1052c48bdc2906346611d5a529e82f000724cc0e
queen <queennjoki285@gmail.com>
2025-01-26 21:29:02 +0300

    validate d

--- app.js
+++ app.js
@@ -4,3 +4,10 @@
     const month = parseInt(document.getElementById('month').value);
     const year = parseInt(document.getElementById('year').value);
     const gender = document.getElementById('gender').value;
+
+
+// Validate the date and month
+if (day <= 0 || day > 31 || month <= 0 || month > 12) {
+    alert("Please enter a valid date and month.");
+    return;
+}
