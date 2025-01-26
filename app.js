bf4673095ac41f2eeca83a51ee557cfb1d941510
queen <queennjoki285@gmail.com>
2025-01-26 21:27:46 +0300

    form values

---
+++ app.js
@@ -0,0 +1,6 @@
+
+    // Get the values from the form
+    const day = parseInt(document.getElementById('day').value);
+    const month = parseInt(document.getElementById('month').value);
+    const year = parseInt(document.getElementById('year').value);
+    const gender = document.getElementById('gender').value;
